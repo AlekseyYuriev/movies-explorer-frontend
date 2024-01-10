95263eecc6b0bbbcd50646f1abf1a380500473df
Aleksey Yuriev <alex11-90@yandex.by>
2024-01-10 20:21:41 +0300

    feat: AboutProject section added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from '../Header/Header';
 import Promo from '../Promo/Promo';
+import AboutProject from '../AboutProject/AboutProject';
 
 function App() {
   return (
     <div className="page">
       <Header />
       <Promo />
+      <AboutProject />
     </div>
   );
 }
