757f6440c995797b5356b1b6c41e4884f97ed8f8
Aleksey Yuriev <alex11-90@yandex.by>
2024-01-11 22:08:46 +0300

    feat: Portfolio component added

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -10,7 +10,7 @@ export default function AboutMe () {
                <h3 className='about-me__info_name'>Алексей</h3>
                <p className='about-me__info_description'>Фронтенд-разработчик, 33 года</p>
                <p className='about-me__info_text'>Я живу в Минске. Закончил факультет маркетинга в БГЭУ по специальности логистика.</p>
-               <a href="https://github.com/AlekseyYuriev" target="_blank" rel="noopener noreferrer" className="about-me__info_link">Github</a>
+               <a href='https://github.com/AlekseyYuriev' target='_blank' rel='noopener noreferrer' className='about-me__info_link'>Github</a>
             </div>
             <img src={photoStudent} className='about-me__photo' alt='Фото студента' />
          </div>
