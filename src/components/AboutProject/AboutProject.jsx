0f0196c7ce3a5c99ef55cd1262dea90fd84907e5
Aleksey Yuriev <alex11-90@yandex.by>
2024-01-24 01:13:54 +0300

    feat: media query done

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -6,8 +6,8 @@ export default function AboutProject () {
          <h2 className='about-project__title'>О проекте</h2>
          <div className='about-project__content'>
             <h3 className='about-project__content_title'>Дипломный проект включал 5 этапов</h3>
-            <h3 className='about-project__content_title'>На выполнение диплома ушло 5 недель</h3>
             <p className='about-project__content_text'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+            <h3 className='about-project__content_title'>На выполнение диплома ушло 5 недель</h3>
             <p className='about-project__content_text'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
          </div>
          <div className='about-project__duration'>
--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -4,6 +4,7 @@ import Header from "../Header/Header";
 import SearchForm from "../SearchForm/SearchForm";
 import MoviesCard from "../MoviesCard/MoviesCard";
 import cards from "../../utils/cards";
+import Footer from "../Footer/Footer";
 
 export default function SavedMovies ({ loggedIn }) {
    return (
@@ -17,6 +18,7 @@ export default function SavedMovies ({ loggedIn }) {
                )}
             </div>
          </section>
+         <Footer />
       </>
    )
 }
