48278f2e2c3ed44e801304b8687827abe4b877a5
Aleksey Yuriev <alex11-90@yandex.by>
2024-01-25 22:58:09 +0300

    feat: MainApi created, register, login, updateUser

---
+++ src/utils/MainApi.js
@@ -0,0 +1,54 @@
+import { API_URL } from "./constants";
+
+export async function getResponseData(res) {
+   if (res.ok) {
+      return res.json();
+   }
+
+   return Promise.reject(new Error(`Ошибка: ${res.status}`));
+}
+
+export async function register(name, email, password) {
+   const request = await fetch(`${API_URL}/signup`, {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, password }),
+   });
+   return getResponseData(request);
+}
+
+export async function login({ email, password }) {
+   const request = await fetch(`${API_URL}/signin`, {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+   });
+   return getResponseData(request);
+}
+
+export async function checkToken(token) {
+   const request = await fetch(`${API_URL}/users/me`, {
+      method: 'GET',
+      headers: {
+         "Content-Type": "application/json",
+         "Authorization": `Bearer ${token}`,
+      },
+   });
+   return getResponseData(request);
+}
+
+export async function updateUser({ name, email }) {
+   const request = await fetch(`${API_URL}/users/me`, {
+      method: 'PATCH',
+      headers: {
+         "Content-Type": "application/json",
+         "Authorization": 'Bearer ' + localStorage.getItem('token'),
+      },
+      body: JSON.stringify({ name, email }),
+   });
+   return getResponseData(request);
+}
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const API_URL = 'https://api.alexmovies.nomoredomainsmonster.ru';
+export const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
